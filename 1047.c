5971e0d8d99a69b2d1f45fc4342f785445436ff4
Ripon <ripon365cse@gmail.com>
2025-05-04 10:59:47 +0600

    solve 1047

--- 1047.c
+++ 1047.c
@@ -2,20 +2,31 @@
 
 int main() {
 
-    int startHour,startMinute;
-    int endHour,endMinute;
-    int startTotal,endTotal;
+    float salary,newSalary,earn;
+    scanf("%f",&salary);
+    int percentage;
 
-    scanf("%d %d %d %d",&startHour,&startMinute,&endHour,&endMinute);
-    startTotal = startHour*60+startMinute;
-    endTotal=endHour*60+endMinute;
-    if(startTotal>=endTotal){
-        endTotal+=24*60;
+    if(salary<=400.00){
+        percentage = 15;
     }
-    int totalduration,totalHour,totalMinute;
-    totalduration=endTotal-startTotal;
-    totalHour=totalduration/60;
-    totalMinute=totalduration%60;
-    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n",totalHour,totalMinute);
+    else if(salary<=800.00)
+    {
+        percentage =12;
+    }
+    else if(salary<=1200.00){
+        percentage =10;
+    }
+    else if(salary<=2000){
+        percentage=7;
+    }
+    else{
+        percentage=4;
+    }
+    newSalary=salary + salary*percentage/100;
+    earn=newSalary-salary;
+
+    printf("Novo salario: %.2f\n",newSalary);
+    printf("Reajuste ganho: %.2f\n",earn);
+    printf("Em percentual: %d %%\n",percentage);
     return 0;
 }
